d0f0514188c6933b78149bd84c241fe757db52b7
Varuagme <57834469+GauravBhattarai@users.noreply.github.com>
2021-12-20 16:26:47 +0545

    Add files via upload

---
+++ Simple Interest/Simple Interest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Simple_Interest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Simple Interest Calculation\n")
+            Console.Write("Principle: ");
+            double p = double.Parse(Console.ReadLine());
+
+            Console.Write("Time: ");
+            double t = double.Parse(Console.ReadLine());
+
+            Console.Write("Rate: ");
+            double r = double.Parse(Console.ReadLine());
+
+            double i = p * t * r / 100;
+            Console.WriteLine($"Interest= {i}");
+
+        }
+    }
+}
