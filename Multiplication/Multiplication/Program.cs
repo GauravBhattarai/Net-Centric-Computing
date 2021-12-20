d8271f16b23f606b60f76c2d44a6246cc62951b8
Varuagme <57834469+GauravBhattarai@users.noreply.github.com>
2021-12-20 16:32:49 +0545

    Add files via upload

---
+++ Multiplication/Multiplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Multiplication Table Generation\n");
+            int i;
+            Console.Write("Enter the number for multiplication: ");
+            double n = double.Parse(Console.ReadLine());
+
+            for(i=1; i<=10; i++)
+                {
+                    Console.Write("{0} * {1} = {2} \n", n, i, n * i);
+                }
+
+        }
+    }
+}
