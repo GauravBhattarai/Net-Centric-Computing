9a4b6a08f6fdd2084f8fd96fe43e04b6671b9cf1
Varuagme <57834469+GauravBhattarai@users.noreply.github.com>
2021-12-20 16:28:21 +0545

    Add files via upload

---
+++ Geometric mean/Geometric mean/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Geometric_mean
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Geometric Mean Calculation\n");
+            int[] data = { 5, 10, 15, 20, 25 };
+            int value = 1;
+            for (int i = 0; i < data.Length; i++)
+            {
+                value = value * data[i];
+            }
+            double result = Math.Pow(value, 1.0 / data.Length);
+
+            Console.WriteLine($"Geometric Mean = {result}");
+
+        }
+    }
+}
