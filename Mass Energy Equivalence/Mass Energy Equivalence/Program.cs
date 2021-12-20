6e8965f616488e53aef3e28efc40b6367e3c63d6
Varuagme <57834469+GauravBhattarai@users.noreply.github.com>
2021-12-20 16:26:11 +0545

    Add files via upload

---
+++ Mass Energy Equivalence/Mass Energy Equivalence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mass_Energy_Equivalence
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Mass Energy Equicalence Calculation\n");
+            double c = 3.00e8;
+            Console.Write("Enter the mass of body in kg: ");
+            double m = double.Parse(Console.ReadLine());
+            double e = m * c * c;
+
+            Console.WriteLine($"Energy(E) = {e}J");
+        }
+    }
+}
